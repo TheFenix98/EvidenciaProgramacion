dc383a6e84f0210a9eb91364da8edd9a70e83317
tatato1 <ezequiel.torazzi@gmail.com>
2025-06-06 15:34:05 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,6 +3,10 @@
 def menu_usuarios(usuarios):
     print("Gestion de usuarios no implementada todavia.")
 
+
+if 25 < 9:
+    print("eres menor")
+
+
 def menu_dispositivos(dispositivos):
     print("Gestion de dispositivos no implementada todavia.")
-
