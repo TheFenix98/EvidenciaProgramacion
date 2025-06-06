e9c045d50d8ddadfc0a4ad273124138835e931cd
tatato1 <ezequiel.torazzi@gmail.com>
2025-06-06 15:37:27 -0300

    Corrijo errores en el formulario de registro

--- menu.py
+++ menu.py
@@ -4,9 +4,5 @@ def menu_usuarios(usuarios):
     print("Gestion de usuarios no implementada todavia.")
 
 
-if 25 < 9:
-    print("eres menor")
-
-
 def menu_dispositivos(dispositivos):
     print("Gestion de dispositivos no implementada todavia.")
