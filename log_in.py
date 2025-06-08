a7432d4e41bf8e6b0eba6fb5d3f6f0dccdc59e84
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-06-08 13:22:34 -0300

    Update log_in.py

--- log_in.py
+++ log_in.py
@@ -4,12 +4,12 @@ from base_datos_usuarios import ususarios_creados
 def log_in():
     acceso = "denegado"
     while acceso == "denegado":
-        usuario_ingresado = input("ingraese su nombre de usuario: ")
+        usuario_ingresado = input("ingrese su nombre de usuario: ")
         contraseña_ingresada = input("ingrese una contraseña: ")
-        lista_de_usuarios = ususarios_creados()
+        lista_de_usuarios = usuarios_creados()
         for user in lista_de_usuarios:
             if user["usuario"] == usuario_ingresado and user["clave"] == contraseña_ingresada:
-                print(f"bienveneido {usuario_ingresado}")
+                print(f"bienvenido {usuario_ingresado}")
                 acceso = "permitido"
                 break
         if acceso == "denegado":
