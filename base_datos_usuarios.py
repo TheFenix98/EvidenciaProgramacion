11d8b72719631259c9a76625bc50ff75506a231c
tatato1 <ezequiel.torazzi@gmail.com>
2025-06-08 00:05:23 -0300

    base de datos de usuarios
    una "base de datos" para que tenga usuarios guardados

---
+++ base_datos_usuarios.py
@@ -0,0 +1,8 @@
+def ususarios_creados():
+    usuario_1 = {"usuario": "Benjamin",
+                "clave": "sorrentino1",
+                "rol": "admin"}
+    usuario_2 = {"usuario": "Ezequiel",
+                "clave": "garsa2",
+                "rol": "invitado"}
+    return [usuario_1, usuario_2]
