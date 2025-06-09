7ba7e866d9f3942fde187527624b359ff82c852d
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-06-09 00:05:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 # Modulo principal para la logica del programa
 
-from menu import autenticar_usuario
-from menu import menu_dispositivos_01
+
+from menu import autenticar_rol_usuario
 from log_in import log_in
 from automatizacion import configurar_hora_apagon
-
-
-print("Bienvenido a la casa intelignete del 'grupo 6'")
-
-log_in()
-
 
 def main():
     usuarios = []
     dispositivos = []
 
+    print("Bienvenido a la casa inteligente del 'grupo 6'")
+
+    infoUsuario = log_in()
+
     while True:
-        print("\n=== Casa Inteligente - Menu Principal ===")
+        print("\n=== Casa Inteligente - Menú Principal ===")
         print("1. Gestionar usuarios")
         print("2. Gestionar dispositivos")
         print("3. Salir")
-        opcion = input("Seleccione una opcion: ")
+        opcion = input("Seleccione una opción: ")
 
-        if opcion == "1":
-            autenticar_usuario(usuarios)
-        elif opcion == "2":
-            configurar_hora_apagon()
-            menu_dispositivos_01(dispositivos)
-        elif opcion == "3":
+        if opcion == "3":
             print("Saliendo del programa.")
             break
-        else:
-            print("La opcion no valida. Intente nuevamente.")
-    
 
-
+        autenticar_rol_usuario(infoUsuario, usuarios, dispositivos, opcion)
 
 if __name__ == "__main__":
     main()
